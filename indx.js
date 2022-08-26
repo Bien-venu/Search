25ddcb40df0b3e846e0d108db2163c08f667c481
2Bienvenu <ishimwesibomanabienvenu@gmail.com>
2022-08-26 09:17:18 +0200

    finish the progect

--- indx.js
+++ indx.js
@@ -1,49 +1,24 @@
-// const dy = document.querySelectorAll("#see");
-// const df = document.getElementById("search");
-// const ins = document.getElementById("in");
-// const btn = document.getElementById("btn");
+const dy = document.querySelectorAll("#see");
+const df = document.getElementById("search");
+const firstInput = document.getElementById("in");
+const theBtn = document.getElementById("btn");
+const secondInput = document.getElementById("didi");
 
-// ins.addEventListener("keyup", () => {
-//   for (i of dy) {
-//     const eve = i.innerHTML;
-//     const vali = ins.value;
-//     {
-//       eve.includes(vali) ? (i.style.display = "") : (i.style.display = "none");
-//     }
-//   }
-// });
-
-// btn.addEventListener("click", () => {
-//   dy.forEach((e) => {
-//     const eve = e.innerHTML;
-//     const vali = ins.value;
-//     {
-//       eve.includes(vali) ? (e.style.display = "") : (e.style.display = "none");
-//     }
-//   });
-// });
-
-// btn.addEventListener("click", () => {
-//   dy.forEach((e) => {
-//     const eve = e.innerHTML;
-//     const vali = ins.value;
-
-//     {
-//       eve == vali ? (e.style.display = "") : (e.style.display = "none");
-//     }
-//   });
-// });
-
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    isReady = [true, false][Math.floor(Math.random() * 2)];
-    isReady ? resolve("yes sir") : reject(" ");
-  }, 1000);
+firstInput.addEventListener("keyup", () => {
+  for (i of dy) {
+    const eve = i.innerHTML;
+    const vali = firstInput.value.toLowerCase();
+    {
+      eve.includes(vali) ? (i.style.display = "") : (i.style.display = "none");
+    }
+  }
 });
-
-const didi = async () => {
-  const re = await promise1;
-  console.log(re);
-};
-
-didi();
+theBtn.addEventListener("click", () => {
+  dy.forEach((e) => {
+    const eve = e.innerHTML;
+    const vali = secondInput.value.toLowerCase();
+    {
+      eve.includes(vali) ? (e.style.display = "") : (e.style.display = "none");
+    }
+  });
+});
